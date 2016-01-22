6ed45ab5970adb1ad9d69500b83d26df61c61fb5
Evy Ily <Evy Ily>
2016-01-22 08:22:43 +0100

    exo8-listView

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,7 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +19,10 @@ public class LibraryActivity extends AppCompatActivity {
 
         List<Book> books = getBooks();
 
-        // TODO findViewById() and setAdapter()
+        BookAdapter adapter = new BookAdapter(this, books);
+        ListView bookList = (ListView) findViewById(R.id.bookListView);
+        bookList.setAdapter(adapter);
+
     }
 
     private List<Book> getBooks() {
